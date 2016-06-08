6a5b961041c9521707695f60c5d43e5c6bbc7014
Grant Boyle <gboyle@sympatico.ca>
2016-06-08 19:21:11 -0400

    starting another test repository

---
+++ scratch-pad/main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <algorithm> 
+#include <iterator>
+
+int main(int argc, char *argv[]) {
+
+	for (int i = 0; i < argc; ++i) {
+		std::cout << argv[i] << std::endl;
+	}
+
+	return 0;
+}
