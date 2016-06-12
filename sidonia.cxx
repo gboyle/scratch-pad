6edfd41a5bee962b4f67461343bc6606d9335e72
Grant Boyle <grant.boyle@gmail.com>
2016-06-12 18:39:50 -0400

    Add conditional somelib define

--- sidonia.cxx
+++ sidonia.cxx
@@ -3,8 +3,15 @@
 #include "sidonia-config.h"
 #include "somelib.h"
 
+#ifdef USE_SOMELIB
+#include "somelib.h"
+#endif
+
 int main() {
 
     std::cout << "sidonia " << SIDONIA_VERSION_MAJOR << "." << SIDONIA_VERSION_MINOR << std::endl;
+
+    #ifdef USE_SOMELIB
     std::cout << somefun(3.14) << std::endl;
+    #endif
 }
